8f5ff7ab3960398c4c92d0df616f623843598c38
zyl <ElaineZhao92@gmail.com>
2022-05-23 19:59:34 +0800

    modify insert.sql definition

--- backend/SQLDefinition/insert.sql
+++ backend/SQLDefinition/insert.sql
@@ -4,7 +4,7 @@ insert into security_type(securityid,accountType) values(2000000,'corporate');
 
 
 -- 个人证券
-insert into personSecurity(securityid,name,gender,identityid,homeaddress,work,education,workaddress,phone,agentidentityid,state)
+insert into personSecurity(securityid,name,gender,identityid,homeaddress,work,education,workaddress,phone,agentidentityid,accountstate)
 values(1000000,'tester','male','12345678901234567X','zju','student','undergraduate','zju','1234512314','','normal');
 
 -- 法人证券
@@ -13,10 +13,10 @@ values(2000000,1,'license','000000000000000000','tester','123456','zju','higherT
 
 
 -- 个人资金
-insert into capitalaccount values(1000,'12345678901234567X',100000,1000,1234,1234,0,'normal',0);
+insert into capitalaccount values(1000,'12345678901234567X',1000000,1000,1234,1234,0,'normal',0);
 
 -- 法人资金账户
-insert into capitalaccount values(2000,'000000000000000000',200000,100000,1234,1234,0,'normal',0);
+insert into capitalaccount values(2000,'000000000000000000',2000000,100000,1234,1234,0,'normal',0);
 
 -- 证券管理人员相关信息信息
 insert into securitiesadministrator values('123456','123456');
